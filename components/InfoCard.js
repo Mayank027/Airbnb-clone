df3f7d935bec763aef22a747de83acad8b4ad1d2
Mayank027 <58890700+Mayank027@users.noreply.github.com>
2022-07-10 21:29:49 +0530

    Airbnb-Build-3

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -9,7 +9,7 @@ function InfoCard({img, location, title, description, star, price, total}) {
         <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
             <Image src={img} layout='fill' objectFit='cover' className='rounded-2xl' />
         </div>
-        <div className='flex flex-col flex-grow pl-5'>
+        <div className='flex flex-col flex-grow pl-10'>
             <div className='flex justify-between'>
                 <p>{location}</p>
                 <HeartIcon className='h-7 cursor-pointer active:scale-125 transition transform duration-200' />
