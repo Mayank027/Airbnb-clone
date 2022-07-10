e8b4878f9a8091d9dc7d2f37dce87f0880298732
Mayank027 <58890700+Mayank027@users.noreply.github.com>
2022-07-10 15:46:27 +0530

    Airbnb-Build-3

--- components/Header.js
+++ components/Header.js
@@ -75,7 +75,7 @@ function Header({placeholder}) {
       </div>
 
       {/* Right */}
-      <div className="hidden lg:flex items-center space-x-4 justify-end text-gray-500">
+      <div className="hidden md:flex items-center space-x-4 justify-end text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 p-2 border-2 rounded-full cursor-pointer scale-75 lg:scale-100">
